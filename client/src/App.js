4693e892a22b60c872de82ec95bd78041793e171
jeffdonkey <8jefa8@gmail.com>
2023-02-27 11:55:30 -0500

    jeff home and showspice render start

--- client/src/App.js
+++ client/src/App.js
@@ -9,9 +9,11 @@ import Login from "./components/screens/Login";
 import Register from "./components/screens/Register";
 import ForgotPassword from "./components/screens/ForgotPassword";
 import ResetPassword from "./components/screens/ResetPassword";
+import Home from "./Home";
+import ShowSpice from "./ShowSpice";
 
 
-const App = () => {
+function App() {
     return ( // The Router component wraps the entire app; is used to provide the app with routing functionality
         <Router>
             <div className="app">
@@ -20,18 +22,19 @@ const App = () => {
                 <Switch>
 
                     {/* The Route components render a component when the current URL matches the path prop */}
-                    <Route exact path="/login" component={ Login } />
-                    <Route exact path="/register" component={ Register } />
-                    <Route exact path="/forgotpassword" component={ ForgotPassword } />
-                    <Route exact path="/passwordreset/:resetToken" component={ ResetPassword } />
+                    <Route exact path="/" componet={Home} />
+                    <Route exact path="/login" component={Login} />
+                    <Route exact path="/register" component={Register} />
+                    <Route exact path="/forgotpassword" component={ForgotPassword} />
+                    <Route exact path="/passwordreset/:resetToken" component={ResetPassword} />
 
                     {/* The PrivateRoute component renders a component if the user is logged in, else redirects to login page */}
-                    <PrivateRoute exact path="/" component={ PrivateScreen } />
+                    <PrivateRoute exact path="/" component={PrivateScreen} />
 
                 </Switch>
             </div>
         </Router>
     );
-};
+}
 
 export default App;
--- client/src/Home.js
+++ client/src/Home.js
@@ -1,3 +1,20 @@
-// setup for homepage render here
+import image from "./images/spice_background.jpg";
+
+function Home() {
+  return (
+    <main>
+      <h1>HOME</h1>
+      <div>
+        <img height="500" width="800" src={image} alt="spices" />
+        <div>
+          Photo by <a href="AUTHOR_LINK">Tiard Schulz</a> on <a href="UNSPLASH_LINK">Unsplash</a>
+        </div>
+      </div>
+    </main>
+  );
+}
+
+export default Home;
+
 
 
