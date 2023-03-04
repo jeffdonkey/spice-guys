5eb0ae4c56d193cd4979ea739a8ed8b352a39772
Gang Cao <gangcao@Gangs-MacBook-Pro.local>
2023-03-03 20:00:59 -0500

    updated seed data

--- OLD_BACK/data/spiceSeederData.js
+++ OLD_BACK/data/spiceSeederData.js
@@ -159,38 +159,7 @@ const spiceSeederData = [
     description:
       "The makrut lime is a small, wrinkled lime with bumpy skin that grows in Southeast Asia. Much more potent and bitter than a regular lime, makrut limes are prized for their leaves and rind rather than their juice. The glossy, dark green leaves are responsible for the bright, citrus flavor in many Thai dishes. Best used fresh, makrut lime leaves can also be dried and ground.",
   },
-  {
-    name: "Garlic",
-    tagline: "What's cooking?",
-    image:
-      "https://images.unsplash.com/photo-1558981402-6d6a7b7b7b0d?ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80",
-    description:
-      "Garlic is a species in the onion genus, Allium. Its close relatives include the onion, shallot, leek, chive, and Chinese onion.",
-  },
-  {
-    name: "Garlic",
-    tagline: "What's cooking?",
-    image:
-      "https://images.unsplash.com/photo-1558981402-6d6a7b7b7b0d?ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80",
-    description:
-      "Garlic is a species in the onion genus, Allium. Its close relatives include the onion, shallot, leek, chive, and Chinese onion.",
-  },
-  {
-    name: "Garlic",
-    tagline: "What's cooking?",
-    image:
-      "https://images.unsplash.com/photo-1558981402-6d6a7b7b7b0d?ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80",
-    description:
-      "Garlic is a species in the onion genus, Allium. Its close relatives include the onion, shallot, leek, chive, and Chinese onion.",
-  },
-  {
-    name: "Garlic",
-    tagline: "What's cooking?",
-    image:
-      "https://images.unsplash.com/photo-1558981402-6d6a7b7b7b0d?ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80",
-    description:
-      "Garlic is a species in the onion genus, Allium. Its close relatives include the onion, shallot, leek, chive, and Chinese onion.",
-  },
+  
 ];
 
 module.exports = spiceSeederData;
